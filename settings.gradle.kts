966060634c45f4f41547bc02dfe27c203a0e91b8
0marperez <operez899999@gmail.com>
2025-09-11 13:36:50 -0400

    misc: enable custom ktlint rules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,6 +17,20 @@ pluginManagement {
     }
 }
 
+dependencyResolutionManagement {
+    repositories {
+        mavenLocal()
+        mavenCentral()
+        maven {
+            name = "kotlinRepoTools"
+            url = java.net.URI("https://d2gys1nrxnjnyg.cloudfront.net/releases")
+            content {
+                includeGroupByRegex("""aws\.sdk\.kotlin.*""")
+            }
+        }
+    }
+}
+
 includeBuild("./build-support")
 
 rootProject.name = "aws-crt-kotlin-parent"
