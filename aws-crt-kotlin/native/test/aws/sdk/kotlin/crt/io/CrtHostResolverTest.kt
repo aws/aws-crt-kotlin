873d3b102e14974e2a87287d1a622686f7c3570e
Matas Lauzadis <lauzmata@amazon.com>
2025-09-19 15:29:28 -0400

    Safer tests

--- aws-crt-kotlin/native/test/aws/sdk/kotlin/crt/io/CrtHostResolverTest.kt
+++ aws-crt-kotlin/native/test/aws/sdk/kotlin/crt/io/CrtHostResolverTest.kt
@@ -5,6 +5,7 @@
 
 package aws.sdk.kotlin.crt.io
 
+import aws.sdk.kotlin.crt.use
 import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -16,54 +17,61 @@ import kotlin.test.assertTrue
 class CrtHostResolverTest {
     @Test
     fun testResolveLocalhost() = runTest {
-        val hr = HostResolver()
-        val addresses = hr.resolve("localhost")
-        assertTrue(addresses.isNotEmpty())
+        HostResolver().use {
+            val addresses = it.resolve("localhost")
+            assertTrue(addresses.isNotEmpty())
 
-        addresses.forEach { addr ->
-            assertEquals("localhost", addr.host)
+            addresses.forEach { addr ->
+                assertEquals("localhost", addr.host)
 
-            val localHostAddress = when (addr.addressType) {
-                AddressType.IpV4 -> "127.0.0.1"
-                AddressType.IpV6 -> "::1"
+                val localHostAddress = when (addr.addressType) {
+                    AddressType.IpV4 -> "127.0.0.1"
+                    AddressType.IpV6 -> "::1"
+                }
+                assertEquals(localHostAddress, addr.address)
             }
-            assertEquals(localHostAddress, addr.address)
         }
     }
 
     @Test
     fun testResolveIpv4Address() = runTest {
-        val addresses = HostResolver().resolve("127.0.0.1")
-        assertTrue(addresses.isNotEmpty())
+        HostResolver().use {
+            val addresses = it.resolve("127.0.0.1")
+            assertTrue(addresses.isNotEmpty())
 
-        addresses.forEach { addr ->
-            assertEquals(AddressType.IpV4, addr.addressType)
-            assertEquals("127.0.0.1", addr.address)
+            addresses.forEach { addr ->
+                assertEquals(AddressType.IpV4, addr.addressType)
+                assertEquals("127.0.0.1", addr.address)
+            }
         }
     }
 
     @Test
     fun testResolveIpv6Address() = runTest {
-        val addresses = HostResolver().resolve("::1")
-        assertTrue(addresses.isNotEmpty())
+        HostResolver().use {
+            val addresses = it.resolve("::1")
+            assertTrue(addresses.isNotEmpty())
 
-        addresses.forEach { addr ->
-            assertEquals(AddressType.IpV6, addr.addressType)
-            assertEquals("::1", addr.address)
+            addresses.forEach { addr ->
+                assertEquals(AddressType.IpV6, addr.addressType)
+                assertEquals("::1", addr.address)
+            }
         }
     }
 
     @Test
     fun testResolveExampleDomain() = runTest {
-        val addresses = HostResolver().resolve("example.com")
-        assertNotNull(addresses)
-        assertTrue(addresses.isNotEmpty())
+        HostResolver().use {
+            val addresses = it.resolve("example.com")
+            assertNotNull(addresses)
+            assertTrue(addresses.isNotEmpty())
 
-        addresses.forEach { addr ->
-            assertEquals("example.com", addr.host)
-            when (addr.addressType) {
-                AddressType.IpV4 -> assertEquals(3, addr.address.count { it == '.' })
-                AddressType.IpV6 -> assertEquals(if (addr.address.contains("::")) 6 else 7, addr.address.count { it == ':' })
+            addresses.forEach { addr ->
+                assertEquals("example.com", addr.host)
+                when (addr.addressType) {
+                    AddressType.IpV4 -> assertEquals(3, addr.address.count { it == '.' })
+                    AddressType.IpV6 -> assertEquals(if (addr.address.contains("::")) 6 else 7, addr.address.count { it == ':' })
+                }
             }
         }
     }
@@ -71,7 +79,12 @@ class CrtHostResolverTest {
     @Test
     fun testResolveInvalidDomain() = runTest {
         assertFails {
-            HostResolver().resolve("this-domain-definitely-does-not-exist-12345.local")
+            val hr = HostResolver()
+            try {
+                hr.resolve("this-domain-definitely-does-not-exist-12345.local")
+            } finally {
+                hr.close()
+            }
         }
     }
 }
