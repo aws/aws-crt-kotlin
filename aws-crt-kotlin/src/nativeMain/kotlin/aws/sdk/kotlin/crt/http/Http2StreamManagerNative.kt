46c35dd2b32b93eddab7f127ff94c2e3e3a388c9
Matas Lauzadis <lauzmata@amazon.com>
2026-02-18 15:48:54 -0500

    ktlint

--- aws-crt-kotlin/src/nativeMain/kotlin/aws/sdk/kotlin/crt/http/Http2StreamManagerNative.kt
+++ aws-crt-kotlin/src/nativeMain/kotlin/aws/sdk/kotlin/crt/http/Http2StreamManagerNative.kt
@@ -1,13 +1,19 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package aws.sdk.kotlin.crt.http
 
 public actual class Http2StreamManager actual constructor(options: Http2StreamManagerOptions) :
-    aws.sdk.kotlin.crt.Closeable, aws.sdk.kotlin.crt.AsyncShutdown {
+    aws.sdk.kotlin.crt.Closeable,
+    aws.sdk.kotlin.crt.AsyncShutdown {
     public actual val options: Http2StreamManagerOptions
         get() = TODO("Not yet implemented")
 
     public actual suspend fun acquireStream(
         request: Http2Request,
-        handler: HttpStreamResponseHandler
+        handler: HttpStreamResponseHandler,
     ): Http2Stream {
         TODO("Not yet implemented")
     }
