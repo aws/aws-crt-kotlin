4e1db8ee793e5e9e02c9c83c74844e27b6691dfd
0marperez <operez899999@gmail.com>
2026-02-20 16:11:29 -0500

    fix buffer handling

--- aws-crt-kotlin/native/src/aws/sdk/kotlin/crt/util/hashing/EcdsaNative.kt
+++ aws-crt-kotlin/native/src/aws/sdk/kotlin/crt/util/hashing/EcdsaNative.kt
@@ -1,3 +1,8 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package aws.sdk.kotlin.crt.util.hashing
 
 import aws.sdk.kotlin.crt.Allocator
@@ -6,25 +11,35 @@ import aws.sdk.kotlin.crt.util.asAwsByteCursor
 import kotlinx.cinterop.*
 import libcrt.*
 
-public fun keyPairFromPrivateKey(privateKey: ByteArray): CPointer<aws_ecc_key_pair>? {
-    val cursor = privateKey.usePinned { it.asAwsByteCursor() }
-    return aws_ecc_key_pair_new_from_private_key(Allocator.Default, aws_ecc_curve_name.AWS_CAL_ECDSA_P256, cursor)
-}
+public fun eccKeyPairFromPrivateKey(privateKey: ByteArray): CPointer<aws_ecc_key_pair>? = aws_ecc_key_pair_new_from_private_key(
+    Allocator.Default,
+    aws_ecc_curve_name.AWS_CAL_ECDSA_P256,
+    privateKey.usePinned { it.asAwsByteCursor() },
+)
 
-public fun destroyKeyPair(keyPair: CPointer<aws_ecc_key_pair>) {
+public fun releaseEccKeyPair(keyPair: CPointer<aws_ecc_key_pair>) {
     aws_ecc_key_pair_release(keyPair)
 }
 
-public fun signMessage(keyPair: CPointer<aws_ecc_key_pair>, message: ByteArray): ByteArray {
-    val messageCursor = message.usePinned { it.asAwsByteCursor() }
-    val signature = ByteArray(256)
-    val signatureBuf = signature.usePinned {
-        cValue<aws_byte_buf> {
-            capacity = signature.size.convert()
-            len = 0U
-            buffer = it.addressOf(0).reinterpret()
-        }
+public fun signMessage(keyPair: CPointer<aws_ecc_key_pair>, message: ByteArray): ByteArray = memScoped {
+    val signature = ByteArray(
+        aws_ecc_key_pair_signature_length(keyPair).convert(),
+    )
+
+    return signature.usePinned { pinned ->
+        val signatureBuf = alloc<aws_byte_buf>()
+        signatureBuf.capacity = signature.size.convert()
+        signatureBuf.len = 0U
+        signatureBuf.buffer = pinned.addressOf(0).reinterpret()
+
+        awsAssertOpSuccess(
+            aws_ecc_key_pair_sign_message(
+                keyPair,
+                message.usePinned { it.asAwsByteCursor() },
+                signatureBuf.ptr,
+            ),
+        ) { "Unable to sign message with key pair" }
+
+        signature
     }
-    awsAssertOpSuccess(aws_ecc_key_pair_sign_message(keyPair, messageCursor, signatureBuf)) { "aws_ecc_key_pair_sign_message" }
-    return signature.copyOf(signatureBuf.useContents { len.toInt() })
 }
