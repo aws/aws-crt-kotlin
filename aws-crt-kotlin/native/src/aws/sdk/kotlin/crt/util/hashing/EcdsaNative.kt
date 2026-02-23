e65c1c0f15178e318e3f7c9b5cd03644e733bb0d
0marperez <operez899999@gmail.com>
2026-02-23 13:14:44 -0700

    feedbacl except unit tests

--- aws-crt-kotlin/native/src/aws/sdk/kotlin/crt/util/hashing/EcdsaNative.kt
+++ aws-crt-kotlin/native/src/aws/sdk/kotlin/crt/util/hashing/EcdsaNative.kt
@@ -6,40 +6,56 @@
 package aws.sdk.kotlin.crt.util.hashing
 
 import aws.sdk.kotlin.crt.Allocator
+import aws.sdk.kotlin.crt.Closeable
+import aws.sdk.kotlin.crt.CrtRuntimeException
+import aws.sdk.kotlin.crt.WithCrt
 import aws.sdk.kotlin.crt.awsAssertOpSuccess
 import aws.sdk.kotlin.crt.util.asAwsByteCursor
 import kotlinx.cinterop.*
 import libcrt.*
 
-public fun eccKeyPairFromPrivateKey(privateKey: ByteArray): CPointer<aws_ecc_key_pair>? = aws_ecc_key_pair_new_from_private_key(
-    Allocator.Default,
-    aws_ecc_curve_name.AWS_CAL_ECDSA_P256,
-    privateKey.usePinned { it.asAwsByteCursor() },
-)
+public class EcdsaNative: WithCrt(), Closeable {
+    public lateinit var eccKeyPair: CPointer<aws_ecc_key_pair>
 
-public fun releaseEccKeyPair(keyPair: CPointer<aws_ecc_key_pair>) {
-    aws_ecc_key_pair_release(keyPair)
-}
+    public fun initializeEccKeyPairFromPrivateKey(privateKey: ByteArray) {
+        privateKey.usePinned { pinnedPrivateKey ->
+            eccKeyPair = aws_ecc_key_pair_new_from_private_key(
+                Allocator.Default,
+                aws_ecc_curve_name.AWS_CAL_ECDSA_P256,
+                pinnedPrivateKey.asAwsByteCursor(),
+            ) ?: throw CrtRuntimeException("Failed to create ECC key pair from private key")
+        }
+    }
 
-public fun signMessage(keyPair: CPointer<aws_ecc_key_pair>, message: ByteArray): ByteArray = memScoped {
-    val signature = ByteArray(
-        aws_ecc_key_pair_signature_length(keyPair).convert(),
-    )
+    public override fun close() {
+        if (this::eccKeyPair.isInitialized) {
+            aws_ecc_key_pair_release(eccKeyPair)
+        }
+    }
 
-    return signature.usePinned { pinned ->
-        val signatureBuf = alloc<aws_byte_buf>()
-        signatureBuf.capacity = signature.size.convert()
-        signatureBuf.len = 0U
-        signatureBuf.buffer = pinned.addressOf(0).reinterpret()
+    public fun signMessage(message: ByteArray): ByteArray = memScoped {
+        val signature = ByteArray(
+            aws_ecc_key_pair_signature_length(eccKeyPair).convert(),
+        )
 
-        awsAssertOpSuccess(
-            aws_ecc_key_pair_sign_message(
-                keyPair,
-                message.usePinned { it.asAwsByteCursor() },
-                signatureBuf.ptr,
-            ),
-        ) { "Unable to sign message with key pair" }
+        return signature.usePinned { pinnedSignature ->
+            val signatureBuf = alloc<aws_byte_buf>()
+            signatureBuf.capacity = signature.size.convert()
+            signatureBuf.len = 0U
+            signatureBuf.buffer = pinnedSignature.addressOf(0).reinterpret()
 
-        signature
+            message.usePinned { pinnedMessage ->
+                awsAssertOpSuccess(
+                    aws_ecc_key_pair_sign_message(
+                        eccKeyPair,
+                        pinnedMessage.asAwsByteCursor(),
+                        signatureBuf.ptr,
+                    ),
+                ) { "Unable to sign message with key pair" }
+            }
+
+            signature
+        }
     }
 }
+
